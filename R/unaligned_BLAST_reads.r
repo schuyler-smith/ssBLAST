32bdf0f6f6b7a50bbc4971094ad3611fe826d281
Schuyler Smith <sdsmith@iastate.edu>
2019-12-12 15:16:04 -0600

    commit to test installation

---
+++ R/unaligned_BLAST_reads.r
@@ -0,0 +1,69 @@
+#' Extract reads not aligned in BLAST
+#'
+#' Compares fastq file with corresponding BLAST file to find reads that were not found in BLAST search and writes to new file.
+
+#' @usage unaligned_BLAST(fastq_file_path, BLAST_file_path, output_path = NULL)
+#' @param fastq_file_path String that either represents a path to a single fastq file, or a directory containing multiple fastq files.
+#' @param BLAST_file_path String that either represents a path to a single blast file, or a directory containing multiple blast files that correspond to `the fastq_file_path`.
+#' @param output_path String for path that new files should be created. Default uses the `fastq_file_path`.
+#' @return fastq
+#' @useDynLib ssBLAST
+#' @importFrom data.table fread
+#' @export
+#' 
+
+# Sys.setenv("PKG_LIBS"="-lboost_iostreams")
+unaligned_BLAST <- function(fastq_file_path, BLAST_file_path, output_path = NULL){
+  
+  if(length(fastq_file_path) == 1 && dir.exists(fastq_file_path)){ 
+    fastq_file_path <- gsub('/$','',fastq_file_path)
+    fastq_file <- vector()
+    for(extension in c(".fastq.gz$", ".fastq.bz2$", ".fastq$")){
+      fastq_file <- append(fastq_file, dir(fastq_file_path, extension, full.names = TRUE))    
+    }
+  } else {
+    fastq_file <- normalizePath(fastq_file_path)
+  }
+  if(length(BLAST_file_path) == 1 && dir.exists(BLAST_file_path)){ 
+    BLAST_file_path <- gsub('/$','',BLAST_file_path)
+    BLAST_file <- dir(BLAST_file_path, full.names = TRUE)
+  } else {
+    BLAST_file <- normalizePath(BLAST_file_path)
+  }
+  if(length(fastq_file) > 1 || length(BLAST_file) > 1){
+    if(length(fastq_file) != length(BLAST_file)){
+      warning('Number of submitted FASTQ and BLAST files are not equal\nonly files with corresponding names will be used.')
+      fastq_file <- fastq_file[basename(sapply(strsplit(fastq_file,'\\.'),"[[", 1)) %in% basename(sapply(strsplit(BLAST_file,'\\.'),"[[", 1))]
+      BLAST_file <- BLAST_file[basename(sapply(strsplit(BLAST_file,'\\.'),"[[", 1)) %in% basename(sapply(strsplit(fastq_file,'\\.'),"[[", 1))]
+      if(length(fastq_file) != length(BLAST_file) || length(BLAST_file) == 0 || length(fastq_file) == 0){
+        stop('All fastq files should have a corresponding BLAST file.')
+      }
+    }
+  }
+  if(!is.null(output_path)){
+    if(dir.exists(output_path)){
+      for(file in seq_along(fastq_file)){
+        unaligned_BLAST_sequences(fastq_file[file], 
+              unique(data.table::fread(BLAST_file[file])[[1]]), 
+              file.path(gsub('/$','',normalizePath(output_path)), paste("unaligned_", 
+                   basename(sapply(strsplit(fastq_file[file],'\\.'),"[[", 1)), 
+                   ".fastq", sep = '')))
+      } 
+    } else {
+      for(file in seq_along(fastq_file)){
+        unaligned_BLAST_sequences(fastq_file[file], 
+              unique(data.table::fread(BLAST_file[file])[[1]]), 
+              normalizePath(output_path))
+      } 
+    }
+  } else {
+    for(file in seq_along(fastq_file)){
+      unaligned_BLAST_sequences(fastq_file[file], 
+        unique(data.table::fread(BLAST_file[file])[[1]]), 
+        file.path(dirname(fastq_file[file]), paste("unaligned_", 
+                                                   basename(sapply(strsplit(fastq_file[file],'\\.'),"[[", 1)), 
+                                                   ".fastq", sep = '')))
+    } 
+  }
+}
+
---
+++ src/unaligned_BLAST_sequences.cpp
@@ -0,0 +1,80 @@
+/*
+ *
+ *	Author: Schuyler D. Smith
+ *
+ */
+
+#include <Rcpp.h>
+#include <string>
+
+#include <fstream>
+#include <sstream>
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/filter/bzip2.hpp>
+
+void search_BLAST(std::istream& FASTQ_file, Rcpp::StringVector aligned, const char * output_path)
+{
+	remove(output_path);
+	std::string line,
+				seq_id;
+	int lines_processed = 0,
+		seq_line = 0, 
+		BLAST_seq = 0;
+	bool match = false;
+
+	auto output_file = std::fstream(output_path, std::ios::out | std::ios_base::app);
+	std::stringstream output_line(std::ios_base::in | std::ios_base::out);
+	while(std::getline(FASTQ_file, line)) 
+	{
+		if(seq_line == 4){ seq_line = 0; match = false; }
+		if(seq_line > 0 && match){ ++seq_line; continue; }
+		if(seq_line == 0)
+		{
+			seq_id = line.substr(0, line.find(" ", 0));
+			if(strcmp(seq_id.erase(0,1).c_str(), aligned[BLAST_seq]) == 0)
+			{
+				match = true; ++BLAST_seq; ++seq_line; continue;
+			}
+		}
+		output_line << line << "\n";
+		++lines_processed; ++seq_line;
+		if(lines_processed == 1000)
+		{
+			output_file << output_line.rdbuf();
+			output_line.clear(); lines_processed = 0;
+		}
+	}
+	output_file << output_line.rdbuf();
+	output_file.close();
+}
+
+//' @author Schuyler D. Smith
+// [[Rcpp::export]]
+Rcpp::CharacterVector unaligned_BLAST_sequences(
+	std::string FASTQ_file_path,
+	Rcpp::StringVector aligned,
+	const char * output_path
+){
+	std::string ext = FASTQ_file_path.substr(FASTQ_file_path.rfind('.'));
+	if(strcmp(ext.c_str(), ".gz")==0 || strcmp(ext.c_str(), ".bz2")==0) { 
+		std::ifstream file(FASTQ_file_path.c_str(), std::ios_base::in | std::ios_base::binary);
+		if (!file) {Rcpp::Rcout << "Error reading compressed fastq file: " << FASTQ_file_path << "\n"; return R_NilValue;}
+		boost::iostreams::filtering_istream FASTQ_file;
+		if (strcmp(ext.c_str(), ".gz")==0) 
+		{ // gzip FASTQ_file_path
+			FASTQ_file.push(boost::iostreams::gzip_decompressor());
+		} else { // bzip2 FASTQ_file_path
+			FASTQ_file.push(boost::iostreams::bzip2_decompressor());
+		}
+		FASTQ_file.push(file);
+		if(!FASTQ_file) {Rcpp::Rcout << "Error reading fastq file: " << FASTQ_file_path << "\n"; return R_NilValue;}
+		search_BLAST(FASTQ_file, aligned, output_path);
+	} else {
+		std::ifstream FASTQ_file(FASTQ_file_path);
+		search_BLAST(FASTQ_file, aligned, output_path);
+	}
+	
+	return(0);
+}
