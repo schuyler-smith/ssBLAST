a3f0bdf83943551c1369dfed24e53aa6c1222e0f
Schuyler Smith <sdsmith@iastate.edu>
2021-01-12 16:45:40 -0600

    update fasta seq reader for multiline sequences and fix multifile processing

--- src/fasta_seq_names.cpp
+++ src/fasta_seq_names.cpp
@@ -18,7 +18,6 @@
 Rcpp::List fasta_seq_names(
 	Rcpp::StringVector fasta_file_path
 ){
-	Rcpp::StringVector seq_names;
 	std::string 	seq_ID,
 					not_seq_ID,
 					line;
@@ -43,7 +42,8 @@ Rcpp::List fasta_seq_names(
 	Rcpp::List fasta_seqs(n_files);
 	for(size_t i=0; i < n_files; ++i)
 	{
-		std::ifstream 	fasta_file(fasta_files[i]);
+		Rcpp::StringVector seq_names;
+		std::ifstream fasta_file(fasta_files[i]);
 	    while(std::getline(fasta_file, line))
 	    {
 	    	std::stringstream   line_(line);
@@ -65,11 +65,6 @@ return(fasta_seqs);
 Rcpp::List fasta_seq_reads(
 	Rcpp::StringVector fasta_file_path
 ){
-	Rcpp::StringVector seq_names;
-	std::string 	seq_ID,
-					not_seq_ID,
-					line;
-
 	Rcpp::Function directory("dir");
 	Rcpp::Function file_path("normalizePath");
 	Rcpp::Function basename("basename");
@@ -89,19 +84,32 @@ Rcpp::List fasta_seq_reads(
 	size_t n_files = fasta_files.length();
 	Rcpp::List fasta_seqs(n_files);
 	for(size_t i=0; i < n_files; ++i)
-	{
-		std::ifstream 	fasta_file(fasta_files[i]);
+	{	
+		std::string 	read_seq,
+						read_line,
+						line;
+		bool file_start(true);
+		Rcpp::StringVector read_seqs;
+		std::ifstream fasta_file(fasta_files[i]);
 	    while(std::getline(fasta_file, line))
 	    {
-	    	std::stringstream   line_(line);
-	    	std::getline(line_, seq_ID, '\t');
-	    	line_ >> not_seq_ID;
-	    	if(seq_ID[0] != '>')
+	    	std::stringstream line_(line);
+	    	std::getline(line_, read_line, '\t');
+	    	if(read_line[0] == '>')
 	    	{
-	    		seq_names.push_back(seq_ID.erase(0, 1));
+	    		if(!file_start)
+	    		{
+	    			read_seqs.push_back(read_seq);
+	    			read_seq.clear();
+	    		} else {
+		    		file_start = false;	
+	    		}
+	    	} else {
+	    		read_seq.append(read_line);
 	    	}
 	    }
-    	fasta_seqs[i] = seq_names;
+	    read_seqs.push_back(read_seq);
+    	fasta_seqs[i] = read_seqs;
 	}
 	fasta_seqs.attr("names") = namevec;
 return(fasta_seqs);
